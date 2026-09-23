b32c8a7c09994f37371876f4ecd2f021037cb2fe
IT22228208 <It22228208@my.sliit.lk>
2026-09-23 15:31:26 +0530

    Add files via upload

---
+++ Q1/IT22228208Lab8Q1B.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+ 
+public class IT22228208Lab8Q1B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] myArray = new int[5];
+        int[] evenArray = new int[5];
+ 
+        System.out.println("Enter 5 Numbers:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter Number " + (i + 1) + ": ");
+            myArray[i] = scanner.nextInt();
+        }
+ 
+        // Pack even numbers to the front of evenArray
+        int evenIndex = 0;
+        for (int i = 0; i < 5; i++) {
+            if (myArray[i] % 2 == 0) {
+                evenArray[evenIndex] = myArray[i];
+                evenIndex++;
+            }
+        }
+ 
+        System.out.println("myArray Contents:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(myArray[i] + " ");
+        }
+        System.out.println();
+ 
+        System.out.println("evenArray Contents:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(evenArray[i] + " ");
+        }
+        System.out.println();
+ 
+        scanner.close();
+    }
+}
